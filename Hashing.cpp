58203a8540c08572a361618847ca5a927cbf9b6b
Sanawar <abrarkhanmohammadsanawar@gmail.com>
2024-11-23 14:39:00 +0500

    Added Unordered Maps for Candidate Registeration and Displaying Candidates

---
+++ Hashing.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <unordered_map> // Used to include unordered maps.
+                         // Unordered map is similar to Python's dictionary
+                         // It stores data in key-value pair. We can access a value against a key
+using namespace std;
+
+struct Candidate
+{
+    string name;
+    string party;
+    int votes;
+    int id;
+};
+
+unordered_map<int, Candidate> candidateTable;
+
+int registerCandidate(unordered_map<int, Candidate> &candidateTable, int id, string name, string party)
+{
+    // Check if a candidate with the same ID already exists
+    if (candidateTable.find(id) != candidateTable.end())
+    {
+        cout << "Candidate with ID " << id << " already exists!" << endl;
+        return 0;
+    }
+
+    // Insert data if it does not exist already
+    Candidate newCandidate = {name, party, 0, id};
+    candidateTable[id] = newCandidate;
+
+    cout << "Candidate " << name << " registered successfully!" << endl;
+
+    return 1;
+}
+
+void displayCandidates(unordered_map<int, Candidate> &candidateTable)
+{
+    unordered_map<int, Candidate>::iterator iter;
+    for (iter = candidateTable.begin(); iter != candidateTable.end(); iter++)
+    {
+        cout << "Candidate's Name: " << iter->second.name
+             << "\nCandidate's ID: " << iter->second.id
+             << "\nCandidate's Party: " << iter->second.party << endl;
+    }
+}
+
+int main()
+{
+    return 0;
+}
