986310b1d358a1f105c7e0ca251e54cbeb3eda91
Sanawar <abrarkhanmohammadsanawar@gmail.com>
2024-12-02 19:31:23 +0500

    Added the traversal function and made some improvements

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -3,24 +3,50 @@ using namespace std;
 
 struct Node
 {
-    int data;
+    string name;
+    long long int CNIC;
+    int vote;
     Node *next;
 };
+
 class Singlelinklist
 {
 public:
     Node *head;
     Node *tail;
-    Singlelinklist()
+
+    Singlelinklist() // Constructor
     {
         head = NULL;
         tail = NULL;
     }
-    void insertastart(int value)
+
+    void insertatStart(string name, long long int CNIC, int votes) // Function to insert values
     {
         Node *newNode = new Node;
-        newNode->data = value;
+
+        newNode->CNIC = CNIC;
         newNode->next = head;
+        newNode->vote = votes;
+
         head = newNode;
+    }
+
+    void traversal(Node *head) // Function for traversal
+    {
+        Node *temp = new Node;
+        temp = head;
+
+        int count = 1;
+
+        do
+        {
+
+            cout << count << ")" << "Name: " << temp->name << "\nCNIC: " << temp->CNIC << "\nNumber of Votes: " << temp->vote << endl;
+            cout << endl;
+
+            temp = temp->next;
+            count++;
+        } while (temp->next = NULL);
     }
 };
