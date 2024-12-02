26dd2413472b00331e5f729c6cc6605b414a4f7c
Sanawar <abrarkhanmohammadsanawar@gmail.com>
2024-12-02 19:45:21 +0500

    Added the function for deletion

--- singlelinklist.cpp
+++ singlelinklist.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 struct Node
@@ -32,7 +33,38 @@ public:
         head = newNode;
     }
 
-    void traversal(Node *head) // Function for traversal
+    Node *deletion(long long int CNIC) // General function for deletion
+    {
+        if (head == NULL)
+        {
+            return NULL; // Returns NULL if the list is empty
+        }
+
+        else if (head->CNIC == CNIC)
+        {
+            head = head->next; // Head becomes the second node if the 'CNIC to be deleted' is at the head
+            return head;
+        }
+
+        // If the element is not stored by the head node and the list is not also empty
+        Node *current = new Node;
+        Node *prev = new Node;
+
+        current = head;
+        prev = NULL;
+
+        while (current != NULL && current->CNIC != CNIC)
+        {
+            prev = current;
+            current = current->next;
+        }
+
+        // Freeing the connection
+        prev->next = current->next;
+        return head;
+    }
+
+    void traversal() // Function for traversal
     {
         Node *temp = new Node;
         temp = head;
